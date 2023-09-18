55f35e5697205ababc5e80256727933cf20358d7
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-18 19:42:42 +0400

    Update Navbar.tsx

--- portfolio/app/Navbar.tsx
+++ portfolio/app/Navbar.tsx
@@ -1,9 +1,15 @@
 interface props {
   name: string;
+  mode: string;
 }
 
-const Navbar = ({ name }: props) => {
-  return <div>{name}</div>;
+const Navbar = ({ name, mode }: props) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>mode</h1>
+    </div>
+  );
 };
 
 export default Navbar;
