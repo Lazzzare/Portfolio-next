3c6ebc55cd1d5e947843bafbc159f4eaaadcf0a5
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-18 19:44:21 +0400

    Update page.tsx

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Home() {
   return (
-    <div className="container max-w-[1240px] mx-auto p-9">
+    <div className="container max-w-[1200px] mx-auto p-9">
       <Navbar name="Lazare" mode="dark" />
     </div>
   );
