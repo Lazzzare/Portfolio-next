1be6ed37dcde063aff8af1ba65cdd966312b6f2d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-19 12:54:35 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Navbar from "./Navbar";
-
 export default function Home() {
-  return (
-    <div className="container max-w-[1200px] mx-auto p-9">
-      <Navbar />
-    </div>
-  );
+  return null;
 }
