80c79f13d1b627ea52cf9b8f9a505511f488cd60
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-19 12:54:36 +0400

    Update Navbar.tsx

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,23 +1,23 @@
 import Image from "next/image";
 import ProfileImage from "../public/profile.jpg";
-import { BsFillPatchCheckFill, BsFillMoonStarsFill } from "react-icons/bs";
-const Navbar = () => {
-  const menuArray = [
-    {
-      itemTitle: "home",
-    },
-    {
-      itemTitle: "skills",
-    },
-    {
-      itemTitle: "project",
-    },
-    {
-      itemTitle: "contact",
-    },
-  ];
+import {
+  BsFillPatchCheckFill,
+  BsFillMoonStarsFill,
+  BsSun,
+} from "react-icons/bs";
+
+interface props {
+  darkMode: boolean;
+  setDarkMode: (e: boolean) => void;
+}
+
+const Navbar = ({ darkMode, setDarkMode }: props) => {
+  const menuArray = ["home", "skills", "project", "contact"];
+
   return (
-    <div className="w-full flex flex-row justify-between text-black">
+    <div
+      className={`w-full flex flex-row justify-between text-black dark:text-white`}
+    >
       {/* LeftSide */}
       <div className="flex items-center gap-x-2">
         <Image
@@ -25,19 +25,32 @@ const Navbar = () => {
           alt="ProfileImage"
           className="w-[48px] h-[48px] rounded-full object-cover mr-2"
         />
-        <h2 className="font-medium text-lg">Lazzzare</h2>
+        <h2 className="font-medium text-lg cursor-pointer dark:text-white">
+          Lazzzare
+        </h2>
         <BsFillPatchCheckFill className="text-blue-500 w-6 h-6" />
       </div>
+
       {/* RightSide */}
       <div className="flex items-center gap-x-5">
-        {menuArray.map((item) => {
-          return (
-            <>
-              <h3 className="cursor-pointer">{item.itemTitle}</h3>
-            </>
-          );
-        })}
-        <BsFillMoonStarsFill className="w-[20px] h-[20px] ml-3 cursor-pointer" />
+        {menuArray.map((item, index) => (
+          <li key={index} className="list-none">
+            <h3 key={index} className="cursor-pointer">
+              {item}
+            </h3>
+          </li>
+        ))}
+        {darkMode ? (
+          <BsSun
+            onClick={() => setDarkMode(!darkMode)}
+            className="w-[20px] h-[20px] ml-3 cursor-pointer"
+          />
+        ) : (
+          <BsFillMoonStarsFill
+            onClick={() => setDarkMode(!darkMode)}
+            className="w-[20px] h-[20px] ml-3 cursor-pointer"
+          />
+        )}
       </div>
     </div>
   );
