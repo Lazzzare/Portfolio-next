a6b0a94a18dbc084a290648e7f4c41569d68baea
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-25 16:54:37 +0400

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,6 +11,7 @@ import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { HiXMark } from "react-icons/hi2";
 import { useTheme } from "next-themes";
 import { useState } from "react";
+import Home from "../page";
 
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState<boolean>(false);
