128b1280bd50d73d0b0135890387a0f8961a6c65
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-26 19:40:45 +0400

    Update Home.tsx

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -5,21 +5,35 @@ import LatestProjects from "./LatestProjects";
 const Home = () => {
   return (
     <div>
-      <motion.div
-        initial={{ opacity: 0, y: 200 }}
-        animate={{ opacity: 1, y: 10 }}
-        transition={{ duration: 0.3 }}
-      >
-        <div className="flex flex-col pt-40 gap-y-4">
-          <h2 className="font-bold text-3xl text-red-400 uppercase cursor-pointer hover:text-red-300">
+      <div className="flex flex-col pt-20 md:pt-40 gap-y-4">
+        <motion.div
+          initial={{ opacity: 0, x: -500 }}
+          animate={{ opacity: 3, x: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <h2 className="font-bold text-xl md:text-3xl max-w-[450px] text-red-400 uppercase cursor-pointer hover:text-red-300">
             hello, my name is lazare
           </h2>
-          <h1 className="font-bold text-7xl">I make websites.</h1>
-          <p className="text-2xl max-w-[800px] mt-2">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: -500 }}
+          animate={{ opacity: 3, x: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1 className="font-bold text-[32px] md:text-7xl">
+            I make websites.
+          </h1>
+          <p className="text-xl md:text-2xl max-w-[800px] mt-2">
             Hey there! {`I'm`} a passionate front-end developer with a love for
             creating captivating web experiences
           </p>
-          <h3 className="text-red-400 text-xl cursor-pointer hover:text-red-300 max-w-[180px]">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: -500 }}
+          animate={{ opacity: 3, x: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <h3 className="text-red-400 text-xl cursor-pointer hover:text-red-300 max-w-[240px]">
             I love{" "}
             <TypeAnimation
               sequence={[
@@ -33,15 +47,18 @@ const Home = () => {
                 1000,
                 "Tailwind",
                 1000,
+                "FramerMotion",
+                1000,
               ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
           </h3>
-        </div>
-      </motion.div>
-      {/* <LatestProjects /> */}
+        </motion.div>
+      </div>
+
+      <LatestProjects />
     </div>
   );
 };
