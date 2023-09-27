f7507e4a789c7e9566b769bee5e07b35019ee400
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 00:06:44 +0400

    Update FacebookMessage.tsx

--- app/components/FacebookMessage.tsx
+++ app/components/FacebookMessage.tsx
@@ -1,9 +1,20 @@
 "use client";
+import { useEffect } from "react";
 import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMessage = () => {
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: "1711829182636666",
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v12.0",
+      });
+    };
+  }, []);
   return (
-    <div className="bg-black border border-red-600">
+    <div>
       <FacebookProvider appId="1711829182636666" chatSupport>
         <CustomChat pageId="132736456589410" minimized={true} />
       </FacebookProvider>
