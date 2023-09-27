1b42d07387b4f56824fa30022034242b92b4f935
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-27 13:15:39 +0400

    commit

--- app/components/LatestProjects.tsx
+++ app/components/LatestProjects.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import TodoProjectImage from "/app/images/todo.png";
+import TodoProjectImage from "../images/TodoProjectImage.png";
 import { motion } from "framer-motion";
 import {
   BiLogoReact,
