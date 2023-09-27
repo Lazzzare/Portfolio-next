a98a7a1f557918444f46fe33bf314938ffedbbb2
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 00:16:07 +0400

    Update FacebookMessage.tsx

--- app/components/FacebookMessage.tsx
+++ app/components/FacebookMessage.tsx
@@ -3,7 +3,18 @@ import { useEffect } from "react";
 import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMessage = () => {
+  const applyCustomStylesToMessenger = () => {
+    const messengerIframe = document.querySelector(
+      ".fb_customer_chat_bounce_in_v2_no_badge .fb_dialog_content iframe"
+    ) as HTMLElement;
+
+    if (messengerIframe) {
+      messengerIframe.style.backgroundColor = "transparent";
+      messengerIframe.style.border = "none";
+    }
+  };
   useEffect(() => {
+    // Load the Messenger plugin after other content has loaded
     window.fbAsyncInit = function () {
       window.FB.init({
         appId: "1711829182636666",
@@ -11,6 +22,14 @@ const FacebookMessage = () => {
         xfbml: true,
         version: "v12.0",
       });
+
+      // Apply custom styles initially
+      applyCustomStylesToMessenger();
+
+      // Apply custom styles every 5 seconds (adjust the interval as needed)
+      setInterval(() => {
+        applyCustomStylesToMessenger();
+      }, 5000);
     };
   }, []);
   return (
