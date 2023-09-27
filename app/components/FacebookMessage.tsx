49515ff6c54fa7f1ee08f17e1206875b95590c91
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 00:02:20 +0400

    Update FacebookMessage.tsx

--- app/components/FacebookMessage.tsx
+++ app/components/FacebookMessage.tsx
@@ -3,9 +3,11 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 const FacebookMessage = () => {
   return (
-    <FacebookProvider appId="1711829182636666" chatSupport>
-      <CustomChat pageId="132736456589410" minimized={true} />
-    </FacebookProvider>
+    <div className="bg-black">
+      <FacebookProvider appId="1711829182636666" chatSupport>
+        <CustomChat pageId="132736456589410" minimized={true} />
+      </FacebookProvider>
+    </div>
   );
 };
 
