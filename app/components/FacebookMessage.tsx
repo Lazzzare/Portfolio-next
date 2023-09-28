640c158baf00046ba8d26b8dd2373503d67bad90
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 11:44:00 +0400

    Update FacebookMessage.tsx

--- app/components/FacebookMessage.tsx
+++ app/components/FacebookMessage.tsx
@@ -1,23 +1,3 @@
-// import React from "react";
-
-// const FullScreenExample = () => {
-//   const handleFullScreenClick = () => {
-//     const element = document.documentElement;
-
-//     // Check if the browser supports the Fullscreen API
-//     if (document.fullscreenEnabled) {
-//       element.requestFullscreen().catch((err) => {
-//         console.error("Error attempting to enable full screen:", err);
-//       });
-//     } else {
-//       console.error("Fullscreen is not supported by this browser.");
-//     }
-//   };
-
-//   return <div></div>;
-// };
-
-// export default FullScreenExample;
 "use client";
 import { FacebookProvider, CustomChat } from "react-facebook";
 
