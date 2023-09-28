5b5a3d9262e6cdfe8231944115f8b16020114784
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 11:11:52 +0400

    add beautiful animations on projects div

--- app/components/LatestProjects.tsx
+++ app/components/LatestProjects.tsx
@@ -8,7 +8,9 @@ import TodoAppSvelte from "../images/TodoAppSvelte.png";
 import GithubProject from "../images/Github.png";
 import TipCalculatorProject from "../images/TipCalculator.png";
 import { motion } from "framer-motion";
+import { AiFillGithub } from "react-icons/ai";
 import { FiFramer } from "react-icons/fi";
+import { IoLogoVercel } from "react-icons/io5";
 import { SiSvelte } from "react-icons/si";
 import { BsArrowRight } from "react-icons/bs";
 import {
@@ -17,6 +19,8 @@ import {
   BiLogoTailwindCss,
 } from "react-icons/bi";
 import { useTheme } from "next-themes";
+import Link from "next/link";
+
 const LatestProjects = () => {
   const { theme, setTheme } = useTheme();
   return (
@@ -32,21 +36,27 @@ const LatestProjects = () => {
           {/* Projects */}
           <div className="flex flex-col gap-8 md:flex-row justify-between">
             {/* Project 1 */}
-            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer hover:rotate-45">
+            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer group hover:rotate-180">
               <Image
-                className="rounded-xl h-[300px] w-[300px] object-cover"
+                className="rounded-xl inset-0 h-[300px] w-[300px] object-cover opacity-100 group-hover:opacity-20 hover:backdrop-blur-lg duration-300"
                 src={TodoProjectImage}
                 alt="TodoProjectImage"
               />
+              <p
+                className={`absolute top-[140px] right-[20px] hover:backdrop-blur-none group-hover:-rotate-180 text-lg cursor-pointer text-red-300 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300`}
+              >
+                This is a simple yet powerful Todo App built using React,
+                Tailwind CSS, Framer-motion and TypeScript.
+              </p>
               <div className="absolute bottom-24 left-[70px]">
                 <h1
-                  className={`text-bold text-3xl cursor-pointer hover:text-red-300 text-white`}
+                  className={`text-bold text-3xl cursor-pointer text-white opacity-100 group-hover:opacity-0 transition-opacity duration-300`}
                 >
                   Todo App
                 </h1>
               </div>
               {/* Icons */}
-              <div className="absolute flex bottom-3 left-[70px]">
+              <div className="absolute flex bottom-3 left-[70px] opacity-100 group-hover:opacity-0 transition-opacity">
                 <BiLogoReact size={40} className="text-blue-500" />
                 <BiLogoTypescript size={40} className="text-blue-700" />
                 <BiLogoTailwindCss size={40} className="text-blue-400" />
@@ -54,42 +64,48 @@ const LatestProjects = () => {
               </div>
             </div>
             {/* Project 2 */}
-            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer">
+            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer group hover:rotate-180">
               <Image
-                className="rounded-xl h-[300px] w-[300px] object-cover"
+                className="rounded-xl inset-0 h-[300px] w-[300px] object-cover opacity-100 group-hover:opacity-20 hover:backdrop-blur-lg duration-300"
                 src={TicTacToe}
                 alt="image"
-                // width={264}
-                // height={800}
               />
+              <p
+                className={`absolute top-[140px] right-[20px] hover:backdrop-blur-none group-hover:-rotate-180 text-lg cursor-pointer text-red-300 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300`}
+              >
+                This is a simple yet powerful Todo App built using React,
+                Tailwind CSS, Framer-motion and TypeScript.
+              </p>
               <div className="absolute top-6 left-[70px]">
-                <h1 className="text-bold text-3xl cursor-pointer hover:text-red-300 text-white">
+                <h1 className="text-bold text-3xl cursor-pointer text-white opacity-100 group-hover:opacity-0 transition-opacity duration-300">
                   Tic Tac Toe
                 </h1>
               </div>
               {/* Icons */}
-              <div className="absolute flex bottom-3 left-[90px]">
+              <div className="absolute flex bottom-3 left-[90px] opacity-100 group-hover:opacity-0 transition-opacity">
                 <BiLogoReact size={40} className="text-blue-500" />
                 <BiLogoTypescript size={40} className="text-blue-700" />
                 <BiLogoTailwindCss size={40} className="text-blue-400" />
               </div>
             </div>
             {/* Project 3 */}
-            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer">
+            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer group hover:rotate-180">
               <Image
-                className="rounded-xl h-[300px] w-[300px] object-cover"
+                className="rounded-xl inset-0 h-[300px] w-[300px] object-cover opacity-100 group-hover:opacity-20 hover:backdrop-blur-lg duration-300"
                 src={PasswordGenerator}
                 alt="image"
-                // width={264}
-                // height={800}
               />
+              <p className="absolute top-[140px] right-[20px] hover:backdrop-blur-none group-hover:-rotate-180 text-lg cursor-pointer text-red-300 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300">
+                This project aims to provide a simple and customizable password
+                generation tool.
+              </p>
               <div className="absolute top-6 left-[30px]">
-                <h1 className="text-bold text-2xl w-full text-center items-center mx-auto cursor-pointer hover:text-red-300 text-white">
+                <h1 className="text-bold text-2xl w-full text-center items-center mx-auto cursor-pointer opacity-100 group-hover:opacity-0 transition-opacity duration-300 hover:text-red-300 text-white">
                   Password Generator
                 </h1>
               </div>
               {/* Icons */}
-              <div className="absolute flex bottom-3 left-[90px]">
+              <div className="absolute flex bottom-3 left-[90px] group-hover:opacity-0 transition-opacity">
                 <BiLogoReact size={40} className="text-blue-500" />
                 <BiLogoTypescript size={40} className="text-blue-700" />
                 <BiLogoTailwindCss size={40} className="text-blue-400" />
@@ -99,17 +115,19 @@ const LatestProjects = () => {
           {/* Below Projects */}
           <div className="mt-8 flex flex-col gap-8 md:flex-row justify-between">
             {/* Project 4 */}
-            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer">
+            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer group hover:rotate-180">
               <Image
-                className="rounded-xl h-[300px] w-[300px] object-cover"
+                className="rounded-xl inset-0 h-[300px] w-[300px] object-cover opacity-100 group-hover:opacity-20 hover:backdrop-blur-lg duration-300"
                 src={TodoAppSvelte}
                 alt="image"
-                // width={264}
-                // height={800}
               />
-              <div className="absolute top-6 left-[70px]">
+              <p className="absolute top-[140px] right-[20px] hover:backdrop-blur-none group-hover:-rotate-180 text-lg cursor-pointer text-red-300 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300">
+                This Todo App is a simple and efficient task management
+                application built using Svelte, TypeScript, and Tailwind CSS.
+              </p>
+              <div className="absolute top-6 left-[90px]">
                 <h1
-                  className={`text-bold text-3xl cursor-pointer hover:text-red-300 ${
+                  className={`text-bold text-2xl w-full text-center items-center mx-auto cursor-pointer opacity-100 group-hover:opacity-0 transition-opacity duration-300 hover:text-red-300 ${
                     theme === "light" ? "text-black" : "text-black"
                   }`}
                 >
@@ -117,45 +135,66 @@ const LatestProjects = () => {
                 </h1>
               </div>
               {/* Icons */}
-              <div className="absolute flex bottom-3 left-[90px]">
+              <div className="absolute flex bottom-3 left-[90px] group-hover:opacity-0 transition-opacity">
                 <SiSvelte size={40} className="text-orange-600" />
                 <BiLogoTypescript size={40} className="text-blue-700" />
                 <BiLogoTailwindCss size={35} className="text-blue-400" />
               </div>
             </div>
             {/* Project 5 */}
-            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer">
+            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer group hover:rotate-180">
               <Image
-                className="rounded-xl h-[300px] w-[300px] object-cover"
+                className="rounded-xl inset-0 h-[300px] w-[300px] object-cover opacity-100 group-hover:opacity-20 hover:backdrop-blur-lg duration-300"
                 src={GithubProject}
                 alt="image"
-                // width={264}
-                // height={800}
               />
+              <p className="absolute top-[140px] right-[20px] hover:backdrop-blur-none group-hover:-rotate-180 text-lg cursor-pointer text-red-300 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300">
+                The Github-User-API is a simple and user-friendly web
+                application that allows users to retrieve detailed information
+                about a GitHub user based on their username.
+              </p>
+              {/* HoverIconsLink */}
+              <div className="absolute bottom-[170px] left-[80px] gap-[40px] hover:backdrop-blur-none flex flex-row group-hover:-rotate-180 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300">
+                <div className="flex flex-col items-center">
+                  <Link target="_blank" href="https://github-user-api-react.netlify.app/">
+                    <IoLogoVercel size={40} />
+                    <h2>Vercel</h2>
+                  </Link>
+                </div>
+                <div className="flex flex-col items-center">
+                <Link target="_blank" href="https://github.com/Lazzzare/GithubUserAPI-React">
+                  <AiFillGithub size={40} />
+                  <h2>Github</h2>
+                  </Link>
+                </div>
+              </div>
               <div className="absolute top-4 left-[40px]">
-                <h1 className="text-bold text-3xl cursor-pointer hover:text-red-300 text-white">
+                <h1 className="text-bold text-3xl cursor-pointer hover:text-red-300 text-white group-hover:opacity-0 transition-opacity">
                   Github User API
                 </h1>
               </div>
               {/* Icons */}
-              <div className="absolute flex bottom-3 left-[90px]">
+              <div className="absolute flex bottom-3 left-[90px] group-hover:opacity-0 transition-opacity">
                 <BiLogoReact size={40} className="text-blue-500" />
                 <BiLogoTypescript size={40} className="text-blue-700" />
                 <BiLogoTailwindCss size={40} className="text-blue-400" />
               </div>
             </div>
             {/* Project 6 */}
-            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer">
+            <div className="relative h-full w-full hover:scale-105 duration-300 max-w-[300px] cursor-pointer group hover:rotate-180">
               <Image
-                className="rounded-xl h-[300px] w-[300px] object-cover"
+                className="rounded-xl inset-0 h-[300px] w-[300px] object-cover opacity-100 group-hover:opacity-20 hover:backdrop-blur-lg duration-300"
                 src={TipCalculatorProject}
                 alt="image"
-                // width={264}
-                // height={800}
               />
+              <p className="absolute top-[110px] right-[20px] hover:backdrop-blur-none group-hover:-rotate-180 text-lg cursor-pointer text-red-300 opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300">
+                Tip Calculator project is a web application built using React
+                and TypeScript, designed to assist users in calculating tips for
+                various services.
+              </p>
               <div className="absolute top-6 left-[50px]">
                 <h1
-                  className={`text-bold text-3xl cursor-pointer hover:text-red-300 ${
+                  className={`text-bold text-3xl cursor-pointer hover:text-red-300 group-hover:opacity-0 transition-opacity ${
                     theme === "light" ? "text-black" : "text-black"
                   }`}
                 >
@@ -163,7 +202,7 @@ const LatestProjects = () => {
                 </h1>
               </div>
               {/* Icons */}
-              <div className="absolute flex bottom-3 left-[90px]">
+              <div className="absolute flex bottom-3 left-[90px] group-hover:opacity-0 transition-opacity">
                 <BiLogoReact size={40} className="text-blue-500" />
                 <BiLogoTypescript size={40} className="text-blue-700" />
                 <BiLogoTailwindCss size={40} className="text-blue-400" />
