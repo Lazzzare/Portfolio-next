ed6d42e015fe2cf02476484c3c43757fc579126c
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-28 10:05:10 +0400

    Update FacebookMessage.tsx

--- app/components/FacebookMessage.tsx
+++ app/components/FacebookMessage.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+// import React from "react";
 
-const FullScreenExample = () => {
-  const handleFullScreenClick = () => {
-    const element = document.documentElement;
+// const FullScreenExample = () => {
+//   const handleFullScreenClick = () => {
+//     const element = document.documentElement;
 
-    // Check if the browser supports the Fullscreen API
-    if (document.fullscreenEnabled) {
-      element.requestFullscreen().catch((err) => {
-        console.error("Error attempting to enable full screen:", err);
-      });
-    } else {
-      console.error("Fullscreen is not supported by this browser.");
-    }
-  };
+//     // Check if the browser supports the Fullscreen API
+//     if (document.fullscreenEnabled) {
+//       element.requestFullscreen().catch((err) => {
+//         console.error("Error attempting to enable full screen:", err);
+//       });
+//     } else {
+//       console.error("Fullscreen is not supported by this browser.");
+//     }
+//   };
 
-  return <div></div>;
+//   return <div></div>;
+// };
+
+// export default FullScreenExample;
+"use client";
+import { FacebookProvider, CustomChat } from "react-facebook";
+
+const FacebookMessage = () => {
+  return (
+    <FacebookProvider appId="1711829182636666" chatSupport>
+      <CustomChat pageId="132736456589410" minimized={true} />
+    </FacebookProvider>
+  );
 };
 
-export default FullScreenExample;
+export default FacebookMessage;
