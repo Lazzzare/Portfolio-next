73d48e154e24c4738f062c6f5ea7bbfc92ccd6b6
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-05 18:02:54 +0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,10 @@
+"use client"
 import React from "react";
 
 const page = () => {
-  return <div>page for contact</div>;
+  return <div className="mt-32">
+    <h1 className="text-4xl font-bold tracking-wide">Contact</h1>
+  </div>;
 };
 
 export default page;
