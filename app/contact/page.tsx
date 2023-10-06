8ba5fa9182f2d8db3004346885af00be5376fe5e
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-06 14:55:36 +0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,38 @@
-"use client"
+"use client";
 import React from "react";
 
 const page = () => {
-  return <div className="mt-32">
-    <h1 className="text-4xl font-bold tracking-wide">Contact</h1>
-  </div>;
+  return (
+    <div className="mt-32">
+      <h1 className="text-4xl font-bold tracking-wide">Contact</h1>
+      <div className="flex flex-row">
+        <form
+          method="POST"
+          action="https://getform.io/f/193741d2-0b2d-47e1-aa61-d8bfb1002308"
+          className="mt-8 max-w-[600px] w-full flex flex-col space-y-4"
+        >
+          <input className="p-2" type="text" placeholder="Name" name="name" required />
+          <input
+            className="p-2 focus:outline-red-400"
+            type="email"
+            placeholder="Email"
+            name="email"
+            required
+          />
+          <textarea
+            className="p-2"
+            name="message"
+            rows={10}
+            placeholder="Message"
+            required
+          ></textarea>
+          <button className="bg-red-500 font-bold w-full px-8 py-3 text-white items-center mx-auto">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default page;
