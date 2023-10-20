f9ece2cfe95ca6c58bafe2ece18f5f3ff4d0815d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-20 18:26:47 +0400

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -31,64 +31,64 @@ const page = () => {
       </div>
       {/* Skill Icons */}
       <div className="flex flex-row flex-wrap w-full mt-8 gap-10">
-        <div className="bg-slate-200 p-6 rounded-md cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md cursor-pointer">
           <AiFillHtml5 size={100} color={"yellow"} />
         </div>
-        <div className="bg-slate-800 p-6 rounded-md cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md cursor-pointer">
           <BiLogoCss3 size={100} color={"orange"} />
         </div>
         <div className="bg-slate-900 p-6 rounded-md cursor-pointer">
           <BiLogoJavascript size={100} color={"yellow"} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-blue-500 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-500 cursor-pointer">
           <BiLogoReact size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-slate-900 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-slate-900 cursor-pointer">
           <SiNextdotjs size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-red-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-red-600 cursor-pointer">
           <SiSvelte size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-blue-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-600 cursor-pointer">
           <BiLogoTypescript size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-purple-500 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-purple-500 cursor-pointer">
           <BiLogoRedux size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-gray-800 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-gray-800 cursor-pointer">
           <SiExpo size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-slate-800 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-slate-800 cursor-pointer">
           <SiReactrouter size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-purple-800 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-purple-800 cursor-pointer">
           <SiAxios size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-orange-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-orange-600 cursor-pointer">
           <SiPostman size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-blue-400 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-400 cursor-pointer">
           <BiLogoTailwindCss size={100} />
         </div>
-        <div className="bg-slate-500 p-6 rounded-md  text-gray-500 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md  text-gray-500 cursor-pointer">
           <SiStyledcomponents size={100} />
         </div>
-        <div className="bg-slate-500 p-6 rounded-md text-purple-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-purple-600 cursor-pointer">
           <BsFillBootstrapFill size={100} />
         </div>
-        <div className="bg-slate-200 p-6 rounded-md text-blue-500 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-500 cursor-pointer">
           <DiMaterializecss size={100} />
         </div>
-        <div className="bg-slate-300 p-6 rounded-md text-blue-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-600 cursor-pointer">
           <FiFramer size={100} />
         </div>
-        <div className="bg-slate-400 p-6 rounded-md  cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md  cursor-pointer">
           <BiLogoFigma size={100} />
         </div>
-        <div className="bg-slate-600 p-6 rounded-md text-orange-700 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-orange-700 cursor-pointer">
           <BsGit size={100} />
         </div>
-        <div className="bg-slate-700 p-6 rounded-md text-gray-900 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-gray-900 cursor-pointer">
           <AiFillGithub size={100} />
         </div>
       </div>
