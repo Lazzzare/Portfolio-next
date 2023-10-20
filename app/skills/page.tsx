6fc5c9fa764ad58029e49bce9525e3b79bfd775f
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-20 17:23:06 +0400

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { BiLogoJavascript } from "react-icons/bi";
 
 const page = () => {
-  return <div>page from skills</div>;
+  return (
+    <div className="mt-32">
+      <div className="text-4xl font-bold tracking-wide md:items-start md:justify-start md:mx-0 items-center mx-auto flex justify-center">
+        Skills
+      </div>
+      {/* Skill Icons */}
+      <div className="flex flex-row flex-wrap w-full mt-8">
+        <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
+          <BiLogoJavascript />
+        </div>
+        <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
+          <BiLogoJavascript />
+        </div>
+        <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
+          <BiLogoJavascript />
+        </div>
+        <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
+          <BiLogoJavascript />
+        </div>
+        <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
+          <BiLogoJavascript />
+        </div>
+        <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
+          <BiLogoJavascript />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
