b0d8f3b5fab1c0abd68c6d822f04e77b6b97a5c1
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-20 16:28:39 +0400

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -74,12 +74,17 @@ const Navbar = () => {
             </div>
           )}
           {mobileMenu ? (
-            <div className="absolute left-0 right-0 top-0 w-full h-screen flex flex-col items-center mx-auto justify-center bg-black z-10 text-orange-400 hover:text-orange-500">
+            <div className="absolute left-0 right-0 top-0 w-full h-screen flex flex-col items-center mx-auto justify-center bg-[#111827] z-10 text-orange-400 hover:text-orange-500">
               <ul className="flex flex-col items-center mx-auto justify-center">
                 {menuArray.map((menuItem) => {
                   return (
                     <Link key={menuItem.id} href={menuItem.link}>
-                      <li>{menuItem.title}</li>
+                      <li
+                        className="text-4xl tracking-wide font-bold
+                      "
+                      >
+                        {menuItem.title}
+                      </li>
                     </Link>
                   );
                 })}
