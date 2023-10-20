1dde8b74d798966dab0f78f4c7179b14b3d14335
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-20 17:24:35 +0400

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -10,22 +10,22 @@ const page = () => {
       {/* Skill Icons */}
       <div className="flex flex-row flex-wrap w-full mt-8">
         <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
-          <BiLogoJavascript />
+          <BiLogoJavascript size={50} />
         </div>
         <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
-          <BiLogoJavascript />
+          <BiLogoJavascript size={50} />
         </div>
         <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
-          <BiLogoJavascript />
+          <BiLogoJavascript size={50} />
         </div>
         <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
-          <BiLogoJavascript />
+          <BiLogoJavascript size={100} />
         </div>
         <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
-          <BiLogoJavascript />
+          <BiLogoJavascript size={100} />
         </div>
         <div className="w-[50px] h-[50px] rounded-lg border-1 border-slate-500">
-          <BiLogoJavascript />
+          <BiLogoJavascript size={100} />
         </div>
       </div>
     </div>
