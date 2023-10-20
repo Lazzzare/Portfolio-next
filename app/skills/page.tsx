bc11cfe95ebcb8414ff627d32705ab567e2ae166
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-20 18:37:51 +0400

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -43,56 +43,72 @@ const page = () => {
           <BiLogoJavascript size={100} color={"yellow"} />
           <h1>Javascript</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-blue-500 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-500 cursor-pointer flex flex-col items-center text-center">
           <BiLogoReact size={100} />
+          <h1>React</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-white cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-white cursor-pointer flex flex-col items-center text-center">
           <SiNextdotjs size={100} />
+          <h1>Next</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-red-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-red-600 cursor-pointer flex flex-col items-center text-center">
           <SiSvelte size={100} />
+          <h1>Svelte</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-blue-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-600 cursor-pointer flex flex-col items-center text-center">
           <BiLogoTypescript size={100} />
+          <h1>Typescript</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-purple-500 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-purple-500 cursor-pointer flex flex-col items-center text-center">
           <BiLogoRedux size={100} />
+          <h1>Redux</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-blue-300 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-300 cursor-pointer flex flex-col items-center text-center">
           <SiExpo size={100} />
+          <h1>Expo</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-red-400 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-red-400 cursor-pointer flex flex-col items-center text-center">
           <SiReactrouter size={100} />
+          <h1>React-Router</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-purple-800 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-purple-800 cursor-pointer flex flex-col items-center text-center">
           <SiAxios size={100} />
+          <h1>Axios</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-orange-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-orange-600 cursor-pointer flex flex-col items-center text-center">
           <SiPostman size={100} />
+          <h1>Postman</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-blue-400 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-400 cursor-pointer flex flex-col items-center text-center">
           <BiLogoTailwindCss size={100} />
+          <h1>Tailwind</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md  text-purple-400 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md  text-purple-400 cursor-pointer flex flex-col items-center text-center">
           <SiStyledcomponents size={100} />
+          <h1>Styled-components</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-purple-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-purple-600 cursor-pointer flex flex-col items-center text-center">
           <BsFillBootstrapFill size={100} />
+          <h1>Bootstrap</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-blue-500 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-500 cursor-pointer flex flex-col items-center text-center">
           <DiMaterializecss size={100} />
+          <h1>MaterialUI</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-blue-600 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-600 cursor-pointer flex flex-col items-center text-center">
           <FiFramer size={100} />
+          <h1>Framer-motion</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-pink-400  cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-pink-400  cursor-pointer flex flex-col items-center text-center">
           <BiLogoFigma size={100} />
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-orange-700 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-orange-700 cursor-pointer flex flex-col items-center text-center">
           <BsGit size={100} />
+          <h1>Git</h1>
         </div>
-        <div className="bg-slate-900 p-6 rounded-md text-blue-800 cursor-pointer">
+        <div className="bg-slate-900 p-6 rounded-md text-blue-800 cursor-pointer flex flex-col items-center text-center">
           <AiFillGithub size={100} />
+          <h1>Github</h1>
         </div>
       </div>
     </div>
