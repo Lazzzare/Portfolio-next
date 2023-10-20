230d6245069c72fafd690a128c91c4c0809e1bd3
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-20 17:54:51 +0400

    Update page.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-import { BiLogoJavascript, BiLogoCss3, BiLogoReact } from "react-icons/bi";
-import { AiFillHtml5 } from "react-icons/ai";
-import { SiNextdotjs, SiSvelte } from "react-icons/si";
+import {
+  BiLogoJavascript,
+  BiLogoCss3,
+  BiLogoReact,
+  BiLogoTailwindCss,
+  BiLogoTypescript,
+  BiLogoRedux,
+  BiLogoFigma,
+} from "react-icons/bi";
+import { AiFillHtml5, AiFillGithub } from "react-icons/ai";
+import {
+  SiNextdotjs,
+  SiSvelte,
+  SiStyledcomponents,
+  SiAxios,
+  SiReactrouter,
+  SiExpo,
+  SiPostman,
+} from "react-icons/si";
+import { BsFillBootstrapFill } from "react-icons/bs";
+import { DiMaterializecss } from "react-icons/di";
+import { FiFramer } from "react-icons/fi";
+import { BsGit } from "react-icons/bs";
 
 const page = () => {
   return (
@@ -10,24 +30,66 @@ const page = () => {
         Skills
       </div>
       {/* Skill Icons */}
-      <div className="flex flex-row flex-wrap w-full mt-8">
-        <div className="rounded-lg border-[1px] border-slate-500 bg-black">
-          <BiLogoJavascript size={100} color={"yellow"} />
+      <div className="flex flex-row flex-wrap w-full mt-8 space-x-10">
+        <div className="rounded-lg border-[1px]  bg-black cursor-pointer">
+          <AiFillHtml5 size={150} color={"yellow"} />
         </div>
-        <div className="rounded-lg border-[1px] border-slate-500 bg-black ">
-          <AiFillHtml5 size={100} color={"orange"} />
+        <div className="rounded-lg border-[1px]  bg-black cursor-pointer">
+          <BiLogoCss3 size={150} color={"orange"} />
         </div>
-        <div className="rounded-lg border-[1px] border-slate-500 bg-black">
-          <BiLogoCss3 size={100} color={"blue"} />
+        <div className="rounded-lg border-[1px]  bg-black cursor-pointer">
+          <BiLogoJavascript size={150} color={"blue"} />
         </div>
-        <div className="rounded-lg border-[1px] border-slate-500 bg-black text-blue-500">
-          <BiLogoReact size={100} />
+        <div className="rounded-lg border-[1px]  bg-black text-blue-500 cursor-pointer">
+          <BiLogoReact size={150} />
         </div>
-        <div className="rounded-lg border-[1px] border-slate-500 bg-black text-slate-900">
-          <SiNextdotjs size={100} />
+        <div className="rounded-lg border-[1px] bg-black text-slate-900 cursor-pointer">
+          <SiNextdotjs size={150} />
         </div>
-        <div className="rounded-lg border-[1px] border-slate-500 bg-black text-red-600">
-          <SiSvelte size={100} />
+        <div className="rounded-lg border-[1px]  bg-black text-red-600 cursor-pointer">
+          <SiSvelte size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-blue-600 cursor-pointer">
+          <BiLogoTypescript size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-purple-500 cursor-pointer">
+          <BiLogoRedux size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-gray-800 cursor-pointer">
+          <SiExpo size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-slate-800 cursor-pointer">
+          <SiReactrouter size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-purple-800 cursor-pointer">
+          <SiAxios size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-orange-600 cursor-pointer">
+          <SiPostman size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-blue-400 cursor-pointer">
+          <BiLogoTailwindCss size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-gray-500 cursor-pointer">
+          <SiStyledcomponents size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-purple-600 cursor-pointer">
+          <BsFillBootstrapFill size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-blue-500 cursor-pointer">
+          <DiMaterializecss size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-blue-600 cursor-pointer">
+          <FiFramer size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black  cursor-pointer">
+          <BiLogoFigma size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-orange-700 cursor-pointer">
+          <BsGit size={150} />
+        </div>
+        <div className="rounded-lg border-[1px]  bg-black text-gray-900 cursor-pointer">
+          <AiFillGithub size={150} />
         </div>
       </div>
     </div>
